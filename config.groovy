4e83f9a244bfbe7231838ffb4ad135de353c994b
Vinod <vinod.panicker@wipro.com>
2020-04-25 12:45:09 +0530

    Initializd candidate and namespace env variables

--- config.groovy
+++ config.groovy
@@ -1,9 +1,15 @@
 drmanVersion = 'master'
+drmanNamespace = 'DIDman'
+distributionBranch = 'dist'
+candidateBranch = 'candidates'
+candidateRepoVersionNumber = '1'
 environments {
 	local {
 		candidatesApi = 'http://localhost:8080/2'
 	}
 	production {
-		candidatesApi = 'https://raw.githubusercontent.com/DIDman/DRman/candidates/candidates/1'
+		candidatesApi = "https://raw.githubusercontent.com/${DRMAN_NAMESPACE}/DRman/${DRMAN_CANDIDATE_BRANCH}/candidates/${DRMAN_CANDIDATE_REPO_VERSION}"
+		// Default => candidatesApi = 'https://raw.githubusercontent.com/DIDman/DRman/canditates/candidates/1'
+	
 	}
 }
---
+++ run_dockerized.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker build --tag=drman/gradle .
+
+#docker run --rm -it -v $PWD:/usr/src/app -v $HOME/.gradle:/root/.gradle drman/gradle test
+
+docker run \
+  -it \
+  -v $PWD:/usr/src/app \
+  -v $HOME/.gradle:/root/.gradle \
+   --entrypoint=/bin/bash \
+  drman/gradle \
+
+ 
