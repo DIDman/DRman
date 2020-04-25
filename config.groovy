ed2cc127d0719fa1eb7e0521935afdd0cfc0b84d
Vinod <vinod.panicker@wipro.com>
2020-04-25 13:10:14 +0530

    fixed issue stopping build

--- config.groovy
+++ config.groovy
@@ -2,13 +2,13 @@ drmanVersion = 'master'
 drmanNamespace = 'DIDman'
 distributionBranch = 'dist'
 candidateBranch = 'candidates'
-candidateRepoVersionNumber = '1'
+candidateRepoVersion = '1'
 environments {
 	local {
 		candidatesApi = 'http://localhost:8080/2'
 	}
 	production {
-		candidatesApi = "https://raw.githubusercontent.com/${DRMAN_NAMESPACE}/DRman/${DRMAN_CANDIDATE_BRANCH}/candidates/${DRMAN_CANDIDATE_REPO_VERSION}"
+		candidatesApi = 'https://raw.githubusercontent.com/${DRMAN_NAMESPACE}/DRman/${DRMAN_CANDIDATE_BRANCH}/candidates/${DRMAN_CANDIDATE_REPO_VERSION}'
 		// Default => candidatesApi = 'https://raw.githubusercontent.com/DIDman/DRman/canditates/candidates/1'
 	
 	}
