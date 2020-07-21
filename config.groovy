4ab248b39c4d473a25caccbc3bc9c00be27c85e0
arunprakashpj <arunprakashpj@gmail.com>
2020-07-22 01:18:05 +0200

    Update config file with production path

--- config.groovy
+++ config.groovy
@@ -8,7 +8,7 @@ environments {
 		candidatesApi = 'http://localhost:8080/2'
 	}
 	production {
-		candidatesApi = 'https://raw.githubusercontent.com/${DRMAN_NAMESPACE}/DRman/blob/${DRMAN_CANDIDATE_BRANCH}/${distributionBranch}/${DRMAN_CANDIDATE_REPO_VERSION}'
+		candidatesApi = 'https://raw.githubusercontent.com/${DRMAN_NAMESPACE}/DRman/blob/${DRMAN_CANDIDATE_BRANCH}/dist/${DRMAN_CANDIDATE_REPO_VERSION}'
 		// Default => candidatesApi = 'https://raw.githubusercontent.com/DIDman/DRman/blob/canditates/dist/1'
 	
 	}
