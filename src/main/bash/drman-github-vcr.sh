18eb95ecd65f98260f3c58a8b2df12d16cd9f525
arunprakashpj <arunprakashpj@gmail.com>
2020-10-07 00:01:31 +0200

    add support for github vcr
    Github VCR Creation feature is added

---
+++ src/main/bash/drman-github-vcr.sh
@@ -0,0 +1,23 @@
+# main menu
+PS3='Please choose your target VCR: '
+options=("Create" "Delete" "Other")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Create")
+            echo "Lets Create Github VCR"
+            sh ./create-github-vcr.sh
+            ;;
+        "Delete")
+            echo "You opeted to delete Gitlab VCR"
+              sh ./delete-github-vcr.sh           
+            ;;
+        "Other")
+            echo "To be filled"        
+            ;;
+         "Quit")
+            exit
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
